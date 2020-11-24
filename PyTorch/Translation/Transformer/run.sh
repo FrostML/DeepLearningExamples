9f03dacbe5678065d45d60d97f448e8ec1998948
FrostML <380185688@qq.com>
2020-11-24 01:58:00 +0000

    add run.sh shell

---
+++ PyTorch/Translation/Transformer/run.sh
@@ -0,0 +1,39 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export CUDA_VISIBLE_DEVICES=2,3
+
+python -m torch.distributed.launch --nproc_per_node 2 ./train.py ./data/wmt14_en_de_joined_dict \
+  --arch transformer_wmt_en_de_big_t2t \
+  --share-all-embeddings \
+  --optimizer adam \
+  --adam-betas '(0.9, 0.997)' \
+  --adam-eps "1e-9" \
+  --clip-norm 0.0 \
+  --lr-scheduler inverse_sqrt \
+  --warmup-init-lr 0.0 \
+  --warmup-updates 4000 \
+  --lr 0.0006 \
+  --min-lr 0.0 \
+  --dropout 0.1 \
+  --weight-decay 0.0 \
+  --criterion label_smoothed_cross_entropy \
+  --label-smoothing 0.1 \
+  --max-tokens 4096 \
+  --seed 1 \
+  --fuse-layer-norm \
+  --save-dir ./checkpoints/ \
+  --distributed-init-method env://
+
+# --amp \
+# --amp O0 \
